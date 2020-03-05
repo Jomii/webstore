9315fd3c918439d3eb148d5e4aa24544d7723d04
juholehtimaki <juho.lehtimaki1@gmail.com>
2020-03-06 01:41:33 +0200

    usestate and useffect for ConfirmationPage

--- client/src/components/ConfirmationPage.jsx
+++ client/src/components/ConfirmationPage.jsx
@@ -2,7 +2,19 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export const ConfirmationPage = () => {
+  const [item, setItem] = useState();
+
+  useEffect(() => {
+    console.log("fetch item from db?");
+    setItem("item info");
+  }, [item]);
+
   let { id } = useParams();
   console.log(id);
-  return <h1>Confirmation page for item: {id}</h1>;
+  return (
+    <div>
+      <h1>Confirmation page for item: {id}</h1>
+      <p>{item}</p>
+    </div>
+  );
 };
