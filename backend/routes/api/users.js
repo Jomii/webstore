6547c33c135bd0abef243bb4c797f5fb98232d8d
Miika Johansson <miika.johansson@outlook.com>
2020-03-06 20:26:19 +0200

    Fix creating users (copy paste from /controllers)
     - Add checks for correct data in request body
     - Add password encryption

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,30 +1,55 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const api = express.Router();
+const bcrypt = require("bcryptjs");
 const User = require("../../models/user");
 const path = "http://localhost:5000/api/users/";
 
+const saltRounds = 12;
+
 // Create a new user
 api.post("/", (req, res) => {
-  // TODO: add password encryption
-  let newUser = new User({
-    email: req.body.email,
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    password: req.body.password,
-    role: req.body.role
-  });
+  if (
+    !req.body ||
+    !req.body.email ||
+    !req.body.firstName ||
+    !req.body.lastName ||
+    !req.body.password
+  ) {
+    res.sendStatus(400);
+    // Unsafe logging, can expose users password
+    return console.error(
+      `Failed to add a user to the database. Request: ${JSON.stringify(
+        req.body
+      )}`
+    );
+  }
 
-  newUser.save(err => {
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
     if (err) {
       res.sendStatus(500);
       return console.error(err);
     }
 
-    console.log("Inserted a new user to collection");
-    res.status(201);
-    res.location(path + newUser._id);
-    res.json(newUser);
+    let newUser = new User({
+      email: req.body.email,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      password: hash,
+      role: req.body.role
+    });
+
+    newUser.save(err => {
+      if (err) {
+        res.sendStatus(500);
+        return console.error(err);
+      }
+
+      console.log(`Inserted a new user (${newUser.email}) to collection`);
+      res.status(201);
+      res.location(path + newUser._id);
+      res.json(newUser);
+    });
   });
 });
 
