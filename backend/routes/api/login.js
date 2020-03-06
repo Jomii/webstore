6bc55dc05d1c94917d5f82420d336106e271ecc7
Miika Johansson <miika.johansson@outlook.com>
2020-03-06 20:59:00 +0200

    Implement logging in (backend)

---
+++ backend/routes/api/login.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const api = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../../models/user");
+
+const secret = "y64u567y2g4";
+
+// Login
+api.get("/", (req, res) => {
+  if (!req.body || !req.body.email || !req.body.password) {
+    res.sendStatus(401);
+  }
+
+  let email = req.body.email;
+  let password = req.body.password;
+
+  User.findOne(
+    {
+      email: email
+    },
+    (err, user) => {
+      if (err) {
+        return console.error(err);
+      }
+
+      if (user) {
+        bcrypt.compare(password, user.password, (err, result) => {
+          if (result) {
+            jwt.sign(
+              { id: user.id, email: user.email, role: user.role },
+              secret,
+              { algorithm: "HS256" },
+              (err, token) => {
+                res.json({ token: token });
+              }
+            );
+          } else {
+            res.sendStatus(401);
+          }
+        });
+      } else {
+        res.sendStatus(401);
+      }
+    }
+  );
+});
+
+module.exports = api;
