4ee890e6a8ad8baf87710d96f8dc04529b0d1a5c
juholehtimaki <juho.lehtimaki1@gmail.com>
2020-03-07 16:38:06 +0200

    item search

--- client/src/components/MarketPage.jsx
+++ client/src/components/MarketPage.jsx
@@ -3,6 +3,8 @@ import { ListedItem } from "./ListedItem.jsx";
 
 export const MarketPage = () => {
   const [items, setItems] = useState();
+  const [query, setQuery] = useState("");
+  const [filteredItems, setFilteredItems] = useState();
 
   useEffect(() => {
     if (!items) {
@@ -12,22 +14,36 @@ export const MarketPage = () => {
         })
         .then(data => {
           setItems(data.items);
+          setFilteredItems(data.items);
         });
     }
-  }, [items]);
+  }, [items, filteredItems]);
+
+  const filterItems = e => {
+    e.preventDefault();
+    let tempItems = [];
+
+    for (let item of items) {
+      if (item.name.includes(query)) tempItems.push(item);
+    }
+    setFilteredItems(tempItems);
+    setQuery("");
+  };
 
   return (
     <div>
       <div className="row justify-content-center search-container">
         <form className="form-inline search-form">
-          <input type="text" />
-          <button type="submit">Search</button>
+          <input value={query} onChange={e => setQuery(e.target.value)} />
+          <button type="submit" onClick={filterItems}>
+            Search
+          </button>
         </form>
       </div>
       <h1>Items:</h1>
-      {items ? (
+      {filteredItems ? (
         <>
-          {items.map((item, index) => (
+          {filteredItems.map((item, index) => (
             <ListedItem iteminfo={item} key={index} />
           ))}
         </>
