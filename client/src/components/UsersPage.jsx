5ec414be64152e5a8213702e12d31dda50e94c74
Topi Nieminen <topi.nieminen@tuni.fi>
2020-03-13 21:54:26 +0200

    Commented some code in components/UserPage.jsx to make it compile. Seems like it was using some deleted code.

--- client/src/components/UsersPage.jsx
+++ client/src/components/UsersPage.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
-import { setAuthentication } from "../redux/actions.js";
+//import { setAuthentication } from "../redux/actions.js";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 
 export const UsersPage = () => {
-  /*const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const auth = useSelector(state => state.auth);
-  useEffect(() => {
+  /*useEffect(() => {
     if (!auth) {
       fetch("http://localhost:5000/api/users")
         .then(response => {
